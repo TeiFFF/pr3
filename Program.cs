ccaae5bc458117e38e2e0fc49de14e22bef05e67
Student <Student@B2-411-11>
2024-04-25 13:40:44 +0400

    kalk

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿Double Result = 0;
+Console.WriteLine("Введите число A");
+double A = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите число B");
+double B = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите Действие");
+string Distinct  = Console.ReadLine();
+if (Distinct == "+")
+{
+    Result = A + B;
+}
+else if (Distinct == "-")
+{
+    Result = A - B;
+}
+else if (Distinct == "*")
+{
+    Result = A * B;
+}
+else if (Distinct == "/")
+{
+    Result = A / B;
+}
+Console.WriteLine(Result);
